db78f96ff0e0b0d3b4401c396d25ac3e290e8537
Moxvallix Mox <moxvallix@gmail.com>
2025-06-11 16:08:49 +0930

    feat(cards): rebalance starter schemes to be level 2

--- src/data/special_schemes.js
+++ src/data/special_schemes.js
@@ -12,11 +12,11 @@ const SPECIAL_SCHEMES = [
     0,
     0,
   ],
-  ["Pointing One Way and then Running the Other Way", "Deception", "3", "Look over there!", "Starter", null, 0, 0, 0, 0, 0],
-  ["Giving a Generally Menacing Vibe", "Might", "3", "Maybe it's the way you look at your opponents. Or maybe it's just your big d*** energy.", "Starter", 0, 0, 0, 0, 0],
-  ["Clever Quip About Your Opponent's Appearance", "Wit", "3", "You look stoopid!", "Starter", null, 0, 0, 0, 0, 0],
-  ["Whispering So Loudly You May As Well Be Talking", "Sneak", "3", "No you shush!", "Starter", null, 0, 0, 0, 0, 0],
-  ["Wiggling Uncontrollably", "Manoeuvre", "3", null, "Starter", 0, 0, 0, 0, 0],
+  ["Pointing One Way and then Running the Other Way", "Deception", "2", "Look over there!", "Starter", null, 0, 0, 0, 0, 0],
+  ["Giving a Generally Menacing Vibe", "Might", "2", "Maybe it's the way you look at your opponents. Or maybe it's just your big d*** energy.", "Starter", 0, 0, 0, 0, 0],
+  ["Clever Quip About Your Opponent's Appearance", "Wit", "2", "You look stoopid!", "Starter", null, 0, 0, 0, 0, 0],
+  ["Whispering So Loudly You May As Well Be Talking", "Sneak", "2", "No you shush!", "Starter", null, 0, 0, 0, 0, 0],
+  ["Wiggling Uncontrollably", "Manoeuvre", "2", null, "Starter", 0, 0, 0, 0, 0],
 ];
 
 export default SPECIAL_SCHEMES;
